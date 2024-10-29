1f883425132b7eb636c51e1d2f66caae063db9b1
25JT <jhonierlucumimeza@gmial.com>
2024-10-29 15:46:11 -0500

    ver1.3 actualizacion de textos y se extiende el tiempo de espera

--- botwpp.py
+++ botwpp.py
@@ -8,19 +8,19 @@ def enviar_mensajes(telefonos, mensaje):
         kit.sendwhatmsg_instantly(telefono, mensaje)
         
         # Espera un momento después de enviar el mensaje para evitar problemas de sincronización
-        sleep(5)
+        sleep(10)
         pyautogui.hotkey('ctrl', 'w')
 
 telefonos = ['+xxxxxx','+xxxxxx']
 mensaje = (
-    "*_¡Hola ¿Cómo estás?_* 🖐️\n"
-    "Hoy quiero invitarte a descubrir una oportunidad increíble para aumentar tus ingresos con *YANBAL*💸\n"
+    "*_¡Hola! ¿Te gustaría ganar más dinero?_* 🖐️\n"
+    "Hoy tienes la oportunidad perfecta para comenzar a generar ingresos extra con *YANBAL* 💸\n"
     "🔝 *Gana* entre un *25%* y un *35%*, y si te lo propones, ¡puedes llegar a ganar *autos* y *viajes*! 🚗✈️\n"
     "🎁*Premios* exclusivos para nuevas consultoras.\n"
     "📦*Bonificaciones por cada pedido realizado.*\n"
     "🌍*_Entregamos a cliente final a nivel nacional_*\n"
     "con un monto mínimo $220.000 y entrega al cliente desde $40.000\n"
-    "¡Escríbeme aquí y te enseño cómo empezar! 💬"
+    "¡Escríbeme para comenzar esta aventura juntas!! 💬"
 )
 
 enviar_mensajes(telefonos, mensaje)
